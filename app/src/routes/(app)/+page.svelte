91cc39c393365073a710abe43a246612499d65c2
CodeMaster4711 <cedi27@t-online.de>
2026-04-30 19:45:57 +0200

    feat: added shadcn sidebar

---
+++ app/src/routes/(app)/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+</script>
+
+<header class="flex h-16 shrink-0 items-center gap-2 px-4 border-b">
+	<Sidebar.Trigger class="-ms-1" />
+</header>
+
+<div class="flex flex-1 flex-col gap-4 p-6">
+	<h1 class="text-2xl font-bold">Dashboard</h1>
+</div>
